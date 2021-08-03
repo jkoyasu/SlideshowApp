8b0c8f9b4bb7ba2cce9e304e67dd3b60d9afd6de
j_koyasu <jp19957811@outlook.jp>
2021-08-04 05:00:32 +0900

    jyt

--- SlideshowApp/Second.swift
+++ SlideshowApp/Second.swift
@@ -15,14 +15,11 @@ class Second: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         secondphotoView.image = secondphoto
-        button.backgroundColor = UIColor.white
         button.layer.borderWidth = 1  // 5
-        button.layer.borderColor = UIColor.blue.cgColor  // 6
         button.layer.cornerRadius = 10  // 7
         button.layer.shadowOffset = CGSize(width: 3, height: 3 )  // 8
         button.layer.shadowOpacity = 0.5  // 9
         button.layer.shadowRadius = 10  // 10
-        button.layer.shadowColor = UIColor.gray.cgColor  // 11
     }
     
 
